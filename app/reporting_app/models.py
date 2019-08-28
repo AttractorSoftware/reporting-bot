a24c8b5b802d789e43cffe064c3199de5b0c09c2
Aibek <aibek.abdykasymov@gmail.com>
2019-08-29 01:06:27 +0600

    #19 add task model and it's migrations

--- app/reporting_app/models.py
+++ app/reporting_app/models.py
@@ -8,3 +8,13 @@ class Project(db.Model):
 
     def __repr__(self):
         return f'<id: {self.id}, name: {self.name}>'
+
+
+class Task(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    code = db.Column(db.String(20), unique=True)
+    name = db.Column(db.String(255), unique=True)
+    description = db.Column(db.Text())
+
+    def __repr__(self):
+        return f'<id: {self.id}, name: {self.name}>'
