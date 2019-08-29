9b2eaf8b289406a0e481a377abf74aa9b6a00776
Aibek <aibek.abdykasymov@gmail.com>
2019-08-29 11:16:48 +0600

    #17 add user model and migration

--- app/reporting_app/models.py
+++ app/reporting_app/models.py
@@ -18,3 +18,15 @@ class Task(db.Model):
 
     def __repr__(self):
         return f'<id: {self.id}, name: {self.name}>'
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255))
+    last_name = db.Column(db.String(255))
+    phone = db.Column(db.String(20), unique=True)
+    email = db.Column(db.String(100), unique=True)
+    telegram_id = db.Column(db.String(20), unique=True)
+
+    def __repr__(self):
+        return f'<id: {self.id}, name: {self.name} {self.last_name}>'
