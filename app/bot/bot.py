b8af6e3e6c444c003857b2deae7c957c903ec7d9
Aibek <aibek.abdykasymov@gmail.com>
2019-09-01 22:05:16 +0600

    #20 change telegram api library to telebot

--- app/bot/bot.py
+++ app/bot/bot.py
@@ -1,15 +1,46 @@
 import os
-from telegram.ext import Updater, CommandHandler
+from telebot import TeleBot, types
+
+token = os.environ['TELEGRAM_BOT_TOKEN']
+bot = TeleBot(token)
 
 
-def hello(bot, update):
-    update.message.reply_text(
-        'Hello {}'.format(update.message.from_user.first_name))
+@bot.message_handler(commands=['hello'])
+def hello_handler(message):
+    bot.reply_to(message, 'Howdy, how are you doing?')
 
 
-updater = Updater(os.environ.get('TELEGRAM_BOT_TOKEN'))
+@bot.message_handler(commands=['register'])
+def register_handler(message):
+    markup = types.ReplyKeyboardMarkup(row_width=2, resize_keyboard=True)
+    choice_project_button = types.KeyboardButton('choice project')
+    markup.add(choice_project_button)
+    bot.send_message(message.chat.id, "Please, enter your email:", reply_markup=markup)
 
-updater.dispatcher.add_handler(CommandHandler('hello', hello))
 
-updater.start_polling()
-updater.idle()
+EMAIL_REGEXP = r'([a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+)'
+@bot.message_handler(regexp=EMAIL_REGEXP)
+def email_handler(message):
+    bot.send_message(message.chat.id,
+                     "Registration confirmation! We sent you an email. Please check your mailbox.")
+
+
+CHOICE = 'choice project'
+
+
+def choice_checker(message):
+    return message.text == CHOICE
+
+
+@bot.callback_query_handler(func=lambda message: message == CHOICE)
+def choice_project_hanlder(message):
+    bot.send_message(message.chat.id, 'Please choice your project')
+
+
+@bot.message_handler(content_types=['text'])
+def send_message(message):
+    bot.send_message(message.chat.id, 'Hello my friend. It is mock message.')
+
+
+print('bot started...')
+bot.polling()
