7247c7e5870822f5638391c5e8211ba0fc135d97
Aibek <aibek.abdykasymov@gmail.com>
2019-09-09 15:27:57 +0600

    #25 add admin panel

--- app/reporting_app/app.py
+++ app/reporting_app/app.py
@@ -1,11 +1,15 @@
 from flask import Flask
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
+from flask_admin import Admin
 from .settings import DevConfig
 from .routes import blueprint
+from .admin import init_admin_views
+
 
 db = SQLAlchemy()
 migrate = Migrate()
+admin = Admin(name="reporting", template_mode="bootstrap3")
 
 
 def create_app(config=DevConfig):
@@ -17,4 +21,7 @@ def create_app(config=DevConfig):
 
     db.init_app(app)
     migrate.init_app(app, db)
+
+    admin.init_app(app)
+    init_admin_views(admin, db, models)
     return app
