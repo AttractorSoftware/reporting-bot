eb19d5eb32346986af1094439e3bd7171dcfe176
Aibek <aibek.abdykasymov@gmail.com>
2019-09-10 18:16:13 +0600

    27 delete old migrations and join its to one, add new report model

--- app/reporting_app/models.py
+++ app/reporting_app/models.py
@@ -15,6 +15,9 @@ class Task(db.Model):
     code = db.Column(db.String(20), unique=True)
     name = db.Column(db.String(255), unique=True)
     description = db.Column(db.Text())
+    project_id = db.Column(db.ForeignKey(Project.id))
+
+    project = db.relationship('Project', foreign_keys='Task.project_id')
 
     def __repr__(self):
         return f'<id: {self.id}, name: {self.name}>'
@@ -30,3 +33,18 @@ class User(db.Model):
 
     def __repr__(self):
         return f'<id: {self.id}, name: {self.name} {self.last_name}>'
+
+
+class Report(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    comment = db.Column(db.Text())
+    user_id = db.Column(db.Integer, db.ForeignKey(User.id))
+    task_id = db.Column(db.Integer, db.ForeignKey(Task.id))
+    time_spent = db.Column(db.Integer)
+    status = db.Column(db.String(30))
+
+    user = db.relationship('User', foreign_keys='Report.user_id')
+    task = db.relationship('Task', foreign_keys='Report.task_id')
+
+    # def __repr__(self):
+    #     return f'<id: {self.id}, ticket name: {self.task.code}>'
