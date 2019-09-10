427fc4551aebc7366b6097818a0d9bf58d2bb204
Aibek <aibek.abdykasymov@gmail.com>
2019-09-10 18:42:56 +0600

    27 add admin view to admin

--- app/reporting_app/admin.py
+++ app/reporting_app/admin.py
@@ -5,3 +5,4 @@ def init_admin_views(admin, db, models):
     admin.add_view(ModelView(models.User, db.session))
     admin.add_view(ModelView(models.Task, db.session))
     admin.add_view(ModelView(models.Project, db.session))
+    admin.add_view(ModelView(models.Report, db.session))
