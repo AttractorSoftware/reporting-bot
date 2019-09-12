40d70bea4ef2741a560e604c8a96dc43412fad24
Aibek <aibek.abdykasymov@gmail.com>
2019-09-12 17:30:36 +0600

    29 reformating code

--- app/reporting_app/reports.py
+++ app/reporting_app/reports.py
@@ -1,12 +1,17 @@
 import httplib2
 import apiclient
 from oauth2client.service_account import ServiceAccountCredentials
+from pprint import pprint
 
 
-def htmlColorToJSON(htmlColor):
-    if htmlColor.startswith("#"):
-        htmlColor = htmlColor[1:]
-    return {"red": int(htmlColor[0:2], 16) / 255.0, "green": int(htmlColor[2:4], 16) / 255.0, "blue": int(htmlColor[4:6], 16) / 255.0}
+def html_color_to_json(html_color):
+    if html_color.startswith("#"):
+        html_color = html_color[1:]
+    return {
+        "red": int(html_color[0:2], 16) / 255.0,
+        "green": int(html_color[2:4], 16) / 255.0,
+        "blue": int(html_color[4:6], 16) / 255.0
+    }
 
 
 class SpreadsheetError(Exception):
@@ -22,172 +27,181 @@ class SheetNotSetError(SpreadsheetError):
 
 
 class Spreadsheet:
-    def __init__(self, json_key_file_name, debugMode=False):
-        self.debugMode = debugMode
-        self.credentials = ServiceAccountCredentials.from_json_keyfile_name(json_key_file_name, ['https://www.googleapis.com/auth/spreadsheets',
-                                                                                                 'https://www.googleapis.com/auth/drive'])
-        self.httpAuth = self.credentials.authorize(httplib2.Http())
-        self.service = apiclient.discovery.build('sheets', 'v4', http=self.httpAuth)
-        self.driveService = None  # Needed only for sharing
-        self.spreadsheetId = None
-        self.sheetId = None
-        self.sheetTitle = None
+    def __init__(self, json_key_file_name, debug_mode=False):
+        self.debug_mode = debug_mode
+        api_urls = ['https://www.googleapis.com/auth/spreadsheets', 'https://www.googleapis.com/auth/drive']
+        self.credentials = ServiceAccountCredentials.from_json_keyfile_name(json_key_file_name, api_urls)
+        self.http_auth = self.credentials.authorize(httplib2.Http())
+        self.service = apiclient.discovery.build('sheets', 'v4', http=self.http_auth)
+        self.drive_service = None  # Needed only for sharing
+        self.spread_sheet_id = None
+        self.sheet_id = None
+        self.sheet_title = None
         self.requests = []
-        self.valueRanges = []
+        self.value_ranges = []
 
     # Creates new spreadsheet
-    def create(self, title, sheetTitle, rows=1000, cols=26, locale='en_US', timeZone='Etc/GMT'):
+    def create(self, title, sheet_title, rows=1000, cols=26, locale='en_US', time_zone='Etc/GMT'):
         spreadsheet = self.service.spreadsheets().create(body={
-            'properties': {'title': title, 'locale': locale, 'timeZone': timeZone},
-            'sheets': [{'properties': {'sheetType': 'GRID', 'sheetId': 0, 'title': sheetTitle, 'gridProperties': {'rowCount': rows, 'columnCount': cols}}}]
+            'properties': {'title': title, 'locale': locale, 'timeZone': time_zone},
+            'sheets': [{'properties': {'sheetType': 'GRID', 'sheetId': 0, 'title': sheet_title,
+                                       'gridProperties': {'rowCount': rows, 'columnCount': cols}}}]
         }).execute()
-        if self.debugMode:
+        if self.debug_mode:
             pprint(spreadsheet)
-        self.spreadsheetId = spreadsheet['spreadsheetId']
-        self.sheetId = spreadsheet['sheets'][0]['properties']['sheetId']
-        self.sheetTitle = spreadsheet['sheets'][0]['properties']['title']
+        self.spread_sheet_id = spreadsheet['spreadsheetId']
+        self.sheet_id = spreadsheet['sheets'][0]['properties']['sheetId']
+        self.sheet_title = spreadsheet['sheets'][0]['properties']['title']
 
-    def share(self, shareRequestBody):
-        if self.spreadsheetId is None:
+    def share(self, share_request_body):
+        if self.spread_sheet_id is None:
             raise SpreadsheetNotSetError()
-        if self.driveService is None:
-            self.driveService = apiclient.discovery.build('drive', 'v3', http=self.httpAuth)
-        shareRes = self.driveService.permissions().create(
-            fileId=self.spreadsheetId,
-            body=shareRequestBody,
+        if self.drive_service is None:
+            self.drive_service = apiclient.discovery.build('drive', 'v3', http=self.http_auth)
+        share_res = self.drive_service.permissions().create(
+            fileId=self.spread_sheet_id,
+            body=share_request_body,
             fields='id'
         ).execute()
-        if self.debugMode:
-            pprint(shareRes)
+        if self.debug_mode:
+            pprint(share_res)
 
-    def shareWithEmailForReading(self, email):
+    def share_with_email_for_reading(self, email):
         self.share({'type': 'user', 'role': 'reader', 'emailAddress': email})
 
-    def shareWithEmailForWriting(self, email):
+    def share_with_email_for_writing(self, email):
         self.share({'type': 'user', 'role': 'writer', 'emailAddress': email})
 
-    def shareWithAnybodyForReading(self):
+    def share_with_any_body_for_reading(self):
         self.share({'type': 'anyone', 'role': 'reader'})
 
-    def shareWithAnybodyForWriting(self):
+    def share_with_anybody_for_writing(self):
         self.share({'type': 'anyone', 'role': 'writer'})
 
-    def getSheetURL(self):
-        if self.spreadsheetId is None:
+    def get_sheet_url(self):
+        if self.spread_sheet_id is None:
             raise SpreadsheetNotSetError()
-        if self.sheetId is None:
+        if self.sheet_id is None:
             raise SheetNotSetError()
-        return 'https://docs.google.com/spreadsheets/d/' + self.spreadsheetId + '/edit#gid=' + str(self.sheetId)
+        return 'https://docs.google.com/spreadsheets/d/' + self.spread_sheet_id + '/edit#gid=' + str(self.sheet_id)
 
     # Sets current spreadsheet by id; set current sheet as first sheet of this spreadsheet
-    def setSpreadsheetById(self, spreadsheetId):
-        spreadsheet = self.service.spreadsheets().get(spreadsheetId=spreadsheetId).execute()
-        if self.debugMode:
+    def set_spread_sheet_by_id(self, spread_sheet_id):
+        spreadsheet = self.service.spreadsheets().get(spreadsheetId=spread_sheet_id).execute()
+        if self.debug_mode:
             pprint(spreadsheet)
-        self.spreadsheetId = spreadsheet['spreadsheetId']
-        self.sheetId = spreadsheet['sheets'][0]['properties']['sheetId']
-        self.sheetTitle = spreadsheet['sheets'][0]['properties']['title']
+        self.spread_sheet_id = spreadsheet['spreadsheetId']
+        self.sheet_id = spreadsheet['sheets'][0]['properties']['sheetId']
+        self.sheet_title = spreadsheet['sheets'][0]['properties']['title']
 
     # spreadsheets.batchUpdate and spreadsheets.values.batchUpdate
-    def runPrepared(self, valueInputOption="USER_ENTERED"):
-        if self.spreadsheetId is None:
+    def run_prepared(self, value_input_option="USER_ENTERED"):
+        if self.spread_sheet_id is None:
             raise SpreadsheetNotSetError()
-        upd1Res = {'replies': []}
-        upd2Res = {'responses': []}
+        upd_first_res = {'replies': []}
+        upd_second_res = {'responses': []}
         try:
             if len(self.requests) > 0:
-                upd1Res = self.service.spreadsheets().batchUpdate(spreadsheetId=self.spreadsheetId,
-                                                                  body={"requests": self.requests}).execute()
-                if self.debugMode:
-                    pprint(upd1Res)
-            if len(self.valueRanges) > 0:
-                upd2Res = self.service.spreadsheets().values().batchUpdate(spreadsheetId=self.spreadsheetId, body={"valueInputOption": valueInputOption,
-                                                                                                                   "data": self.valueRanges}).execute()
-                if self.debugMode:
-                    pprint(upd2Res)
+                upd_first_res = self.service.spreadsheets().batchUpdate(
+                    spreadsheetId=self.spread_sheet_id,
+                    body={"requests": self.requests}).execute()
+                if self.debug_mode:
+                    pprint(upd_first_res)
+            if len(self.value_ranges) > 0:
+                upd_second_res = self.service.spreadsheets().values().batchUpdate(
+                    spreadsheetId=self.spread_sheet_id,
+                    body={
+                        "valueInputOption": value_input_option,
+                        "data": self.value_ranges
+                    }).execute()
+
+                if self.debug_mode:
+                    pprint(upd_second_res)
         finally:
             self.requests = []
-            self.valueRanges = []
-        return (upd1Res['replies'], upd2Res['responses'])
+            self.value_ranges = []
+        return (upd_first_res['replies'], upd_second_res['responses'])
 
-    def prepare_addSheet(self, sheetTitle, rows=1000, cols=26):
-        self.requests.append({"addSheet": {"properties": {"title": sheetTitle,
+    def prepare_add_sheet(self, sheet_title, rows=1000, cols=26):
+        self.requests.append({"addSheet": {"properties": {"title": sheet_title,
                                                           'gridProperties': {'rowCount': rows, 'columnCount': cols}}}})
 
     # Adds new sheet to current spreadsheet, sets as current sheet and returns it's id
-    def addSheet(self, sheetTitle, rows=1000, cols=26):
-        if self.spreadsheetId is None:
+    def add_sheet(self, sheet_title, rows=1000, cols=26):
+        if self.spread_sheet_id is None:
             raise SpreadsheetNotSetError()
-        self.prepare_addSheet(sheetTitle, rows, cols)
-        addedSheet = self.runPrepared()[0][0]['addSheet']['properties']
-        self.sheetId = addedSheet['sheetId']
-        self.sheetTitle = addedSheet['title']
-        return self.sheetId
+        self.prepare_add_sheet(sheet_title, rows, cols)
+        added_sheet = self.run_prepared()[0][0]['addSheet']['properties']
+        self.sheet_id = added_sheet['sheetId']
+        self.sheet_title = added_sheet['title']
+        return self.sheet_id
 
     # Converts string range to GridRange of current sheet; examples:
-    #   "A3:B4" -> {sheetId: id of current sheet, startRowIndex: 2, endRowIndex: 4, startColumnIndex: 0, endColumnIndex: 2}
-    #   "A5:B"  -> {sheetId: id of current sheet, startRowIndex: 4, startColumnIndex: 0, endColumnIndex: 2}
-    def toGridRange(self, cellsRange):
-        if self.sheetId is None:
+    # "A3:B4"-> {sheetId: id of current sheet, startRowIndex: 2, endRowIndex: 4, startColumnIndex: 0, endColumnIndex: 2}
+    # "A5:B" -> {sheetId: id of current sheet, startRowIndex: 4, startColumnIndex: 0, endColumnIndex: 2}
+    def to_grid_range(self, cells_range):
+        if self.sheet_id is None:
             raise SheetNotSetError()
-        if isinstance(cellsRange, str):
-            startCell, endCell = cellsRange.split(":")[0:2]
-            cellsRange = {}
-            rangeAZ = range(ord('A'), ord('Z') + 1)
-            if ord(startCell[0]) in rangeAZ:
-                cellsRange["startColumnIndex"] = ord(startCell[0]) - ord('A')
-                startCell = startCell[1:]
-            if ord(endCell[0]) in rangeAZ:
-                cellsRange["endColumnIndex"] = ord(endCell[0]) - ord('A') + 1
-                endCell = endCell[1:]
-            if len(startCell) > 0:
-                cellsRange["startRowIndex"] = int(startCell) - 1
-            if len(endCell) > 0:
-                cellsRange["endRowIndex"] = int(endCell)
-        cellsRange["sheetId"] = self.sheetId
-        return cellsRange
+        if isinstance(cells_range, str):
+            start_cell, end_cell = cells_range.split(":")[0:2]
+            cells_range = {}
+            range_AZ = range(ord('A'), ord('Z') + 1)
+            if ord(start_cell[0]) in range_AZ:
+                cells_range["startColumnIndex"] = ord(start_cell[0]) - ord('A')
+                start_cell = start_cell[1:]
+            if ord(end_cell[0]) in range_AZ:
+                cells_range["endColumnIndex"] = ord(end_cell[0]) - ord('A') + 1
+                end_cell = end_cell[1:]
+            if len(start_cell) > 0:
+                cells_range["startRowIndex"] = int(start_cell) - 1
+            if len(end_cell) > 0:
+                cells_range["endRowIndex"] = int(end_cell)
+        cells_range["sheetId"] = self.sheet_id
+        return cells_range
 
-    def prepare_setDimensionPixelSize(self, dimension, startIndex, endIndex, pixelSize):
-        if self.sheetId is None:
+    def prepare_set_dimension_pixel_size(self, dimension, start_index, end_index, pixel_size):
+        if self.sheet_id is None:
             raise SheetNotSetError()
         self.requests.append({"updateDimensionProperties": {
-            "range": {"sheetId": self.sheetId,
+            "range": {"sheetId": self.sheet_id,
                       "dimension": dimension,
-                      "startIndex": startIndex,
-                      "endIndex": endIndex},
-            "properties": {"pixelSize": pixelSize},
+                      "startIndex": start_index,
+                      "endIndex": end_index},
+            "properties": {"pixelSize": pixel_size},
             "fields": "pixelSize"}})
 
-    def prepare_setColumnsWidth(self, startCol, endCol, width):
-        self.prepare_setDimensionPixelSize("COLUMNS", startCol, endCol + 1, width)
+    def prepare_set_columns_width(self, start_col, end_col, width):
+        self.prepare_set_dimension_pixel_size("COLUMNS", start_col, end_col + 1, width)
 
-    def prepare_setColumnWidth(self, col, width):
-        self.prepare_setColumnsWidth(col, col, width)
+    def prepare_set_column_width(self, col, width):
+        self.prepare_set_columns_width(col, col, width)
 
-    def prepare_setRowsHeight(self, startRow, endRow, height):
-        self.prepare_setDimensionPixelSize("ROWS", startRow, endRow + 1, height)
+    def prepare_set_rows_height(self, startRow, endRow, height):
+        self.prepare_set_dimension_pixel_size("ROWS", startRow, endRow + 1, height)
 
-    def prepare_setRowHeight(self, row, height):
-        self.prepare_setRowsHeight(row, row, height)
+    def prepare_set_row_height(self, row, height):
+        self.prepare_set_rows_height(row, row, height)
 
-    def prepare_setValues(self, cellsRange, values, majorDimension="ROWS"):
-        if self.sheetTitle is None:
+    def prepare_set_values(self, cells_range, values, major_dimension="ROWS"):
+        if self.sheet_title is None:
             raise SheetNotSetError()
-        self.valueRanges.append({"range": self.sheetTitle + "!" + cellsRange,
-                                 "majorDimension": majorDimension, "values": values})
+        self.value_ranges.append({"range": self.sheet_title + "!" + cells_range,
+                                  "majorDimension": major_dimension, "values": values})
 
-    def prepare_mergeCells(self, cellsRange, mergeType="MERGE_ALL"):
-        self.requests.append({"mergeCells": {"range": self.toGridRange(cellsRange), "mergeType": mergeType}})
+    def prepare_merge_cells(self, cells_range, merge_type="MERGE_ALL"):
+        self.requests.append({"mergeCells": {"range": self.to_grid_range(cells_range), "mergeType": merge_type}})
 
-    # formatJSON should be dict with userEnteredFormat to be applied to each cell
-    def prepare_setCellsFormat(self, cellsRange, formatJSON, fields="userEnteredFormat"):
-        self.requests.append({"repeatCell": {"range": self.toGridRange(cellsRange),
-                                             "cell": {"userEnteredFormat": formatJSON}, "fields": fields}})
+    # format_json should be dict with userEnteredFormat to be applied to each cell
+    def prepare_set_cells_format(self, cells_range, format_json, fields="userEnteredFormat"):
+        self.requests.append({"repeatCell": {"range": self.to_grid_range(cells_range),
+                                             "cell": {"userEnteredFormat": format_json}, "fields": fields}})
 
-    # formatsJSON should be list of lists of dicts with userEnteredFormat for each cell in each row
-    def prepare_setCellsFormats(self, cellsRange, formatsJSON, fields="userEnteredFormat"):
-        self.requests.append({"updateCells": {"range": self.toGridRange(cellsRange),
-                                              "rows": [{"values": [{"userEnteredFormat": cellFormat} for cellFormat in rowFormats]} for rowFormats in formatsJSON],
+    # formats_json should be list of lists of dicts with userEnteredFormat for each cell in each row
+    def prepare_set_cells_formats(self, cells_range, formats_json, fields="userEnteredFormat"):
+        self.requests.append({"updateCells": {"range": self.to_grid_range(cells_range),
+                                              "rows": [{"values": [{"userEnteredFormat": cell_format}
+                                                                   for cell_format in row_formats]}
+                                                       for row_formats in formats_json],
                                               "fields": fields}})
 
 
@@ -195,7 +209,9 @@ if __name__ == "__main__":
     CREDENTIALS_FILE = 'data/reporting-bot-google-api.json'
     sheet = Spreadsheet(CREDENTIALS_FILE)
     # sheet.create_sheet('test_report2')
-    # sheet.share_to_user('aibek.abdykasymov@gmail.com')
+    sheet.set_spread_sheet_by_id('1AtVZBWVerxmlxNJCRmOSS_bDj1XdRWYMkIp1OLjI07U')
+    sheet.share_with_email_for_writing('aibek.abdykasymov@gmail.com')
+    sheet.share_with_email_for_writing('andrewshmelyov@gmail.com')
     print('sheet url is: ', sheet.get_sheet_url())
 
     # google_api.show_files()
