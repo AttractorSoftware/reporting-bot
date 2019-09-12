98ad086574a4c061b49f39148ec59c371a5d696c
Andrei Shmelev <andrewshmelyov@gmail.com>
2019-09-12 19:06:33 +0600

    #28 Switching to Webhooks for Telegram Bot

--- app/reporting_app/settings.py
+++ app/reporting_app/settings.py
@@ -6,6 +6,14 @@ class Config(object):
     PROJECT_ROOT = os.path.abspath(os.path.join(APP_DIR, os.pardir))
     ROOT = os.path.abspath(os.path.join(PROJECT_ROOT, os.pardir))
     DATA_DIR = os.path.join(ROOT, 'data')
+    API_TOKEN = os.environ['TELEGRAM_BOT_TOKEN']
+
+    WEBHOOK_HOST = os.environ['WEBHOOK_HOST']
+    WEBHOOK_PORT = 443  # 443, 80, 88 or 8443 (port need to be 'open')
+    WEBHOOK_LISTEN = '0.0.0.0'  # In some VPS you may need to put here the IP addr
+
+    WEBHOOK_URL_BASE = "https://%s:%s" % (WEBHOOK_HOST, WEBHOOK_PORT)
+    WEBHOOK_URL_PATH = "/%s/" % (API_TOKEN)
 
 
 class ProdConfig(Config):
@@ -19,6 +27,7 @@ class DevConfig(Config):
     SQLALCHEMY_DATABASE_URI = 'sqlite:///{}'.format(os.path.join(Config.DATA_DIR, 'app.sqlite3'))
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = 'development key'
+
 
 class TestConfig(Config):
     ENV = 'test'
