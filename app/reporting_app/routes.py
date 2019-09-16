2359675ab0d467720ea8d365ab79e5de870cee3e
Andrei Shmelev <andrewshmelyov@gmail.com>
2019-09-16 18:38:11 +0600

    #28 With this simplified app initialization we can use DB in submodules
    Finally

--- app/reporting_app/routes.py
+++ app/reporting_app/routes.py
@@ -1,4 +1,7 @@
-from flask import Blueprint
+from flask import Blueprint, request, abort
+from telebot import types
+from .settings import DevConfig as config
+from .app import bot
 
 blueprint = Blueprint('reporting_app', __name__)
 
@@ -6,3 +9,14 @@ blueprint = Blueprint('reporting_app', __name__)
 @blueprint.route('/')
 def index():
     return 'Hello World'
+
+
+@blueprint.route(config.WEBHOOK_URL_PATH, methods=['POST'])
+def webhook():
+    if request.headers.get('content-type') == 'application/json':
+        json_string = request.get_data().decode('utf-8')
+        update = types.Update.de_json(json_string)
+        bot.process_new_updates([update])
+        return ''
+    else:
+        abort(403)
