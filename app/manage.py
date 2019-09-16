9f8071ecfe2f59d210b4c1605b6cfd2d1d0e7cb8
Andrei Shmelev <andrewshmelyov@gmail.com>
2019-09-16 18:38:11 +0600

    #28 With this simplified app initialization we can use DB in submodules
    Finally

--- app/manage.py
+++ app/manage.py
@@ -1,14 +1,11 @@
-from reporting_app.app import create_app
+from reporting_app.app import app
 from flask_script import Manager, Server
 from flask_migrate import MigrateCommand
-from reporting_app.settings import DevConfig
 
-app = create_app(DevConfig)
 manager = Manager(app)
 
 manager.add_command('db', MigrateCommand)
-manager.add_command("runserver", Server(host=app.config['WEBHOOK_LISTEN']))
-
+manager.add_command('runserver', Server(host=app.config['WEBHOOK_LISTEN']))
 
 if __name__ == '__main__':
     manager.run()
--- app/reporting_app/app.py
+++ app/reporting_app/app.py
@@ -1,27 +1,33 @@
+import telebot, time, logging, os
 from flask import Flask
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 from flask_admin import Admin
-from .settings import DevConfig
-from .routes import blueprint
+from .settings import DevConfig as config
+
 from .admin import init_admin_views
 
-
-db = SQLAlchemy()
+bot = telebot.TeleBot(os.environ['TELEGRAM_BOT_TOKEN'])
 migrate = Migrate()
 admin = Admin(name="reporting", template_mode="bootstrap3")
 
+app = Flask(__name__)
+app.config.from_object(config)
+db = SQLAlchemy(app)
+db.init_app(app)
+migrate.init_app(app, db)
+admin.init_app(app)
 
-def create_app(config=DevConfig):
-    app = Flask(__name__)
-    app.config.from_object(config)
-    app.register_blueprint(blueprint)
+from .routes import blueprint
+app.register_blueprint(blueprint)
+from reporting_app import models
+init_admin_views(admin, db, models)
 
-    from reporting_app import models
+bot.remove_webhook()
+time.sleep(0.1)
+bot.set_webhook(url=config.WEBHOOK_URL_BASE + config.WEBHOOK_URL_PATH)
 
-    db.init_app(app)
-    migrate.init_app(app, db)
+logger = telebot.logger
+logger.setLevel(logging.INFO)
 
-    admin.init_app(app)
-    init_admin_views(admin, db, models)
-    return app
+from .bot import handlers
